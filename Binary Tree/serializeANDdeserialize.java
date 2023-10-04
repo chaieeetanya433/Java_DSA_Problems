1052716be385f644df8b1f374301bb01e6a9438d
Chaitanya Agarkar <129309578+chaieeetanya433@users.noreply.github.com>
2023-10-04 21:46:30 +0530

    Add files via upload

---
+++ Binary Tree/serializeANDdeserialize.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+/*********************************************************
+
+ Following is the TreeNode structure:
+
+ class TreeNode {
+     int data;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {
+         this.data = 0;
+         this.left = null;
+         this.right = null;
+     }
+     TreeNode(int data) {
+         this.data = data;
+         this.left = null;
+         this.right = null;
+     }
+     TreeNode(int data, TreeNode left, TreeNode right) {
+         this.data = data;
+         this.left = left;
+         this.right = right;
+     }
+ };
+ ********************************************************/
+
+public class Solution {
+
+    //tc => O(n) & sc => O(n)
+    public static String serializeTree(TreeNode root) {
+        // Write your code here.
+        if(root == null) {
+            return "";
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+        q.add(root);
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if(node == null) {
+                res.append("n ");
+                continue;
+            }
+            res.append(node.data + " ");
+            q.add(node.left);
+            q.add(node.right);
+        }
+        return res.toString();
+    }
+
+    public static TreeNode deserializeTree(String serialized) {
+        // Write your code here.
+        if(serialized == "") return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        String values[] = serialized.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        q.add(root);
+        for(int i=1; i<values.length; i++) {
+            TreeNode parent = q.poll();
+            if(!values[i].equals("n")) {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left = left;
+                q.add(left);
+            }
+            if(!values[++i].equals("n")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                parent.right = right;
+                q.add(right);
+            }
+        }
+        return root;
+    }
+}
