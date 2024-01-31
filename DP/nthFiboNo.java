289c5d4e0ecf635a1a552d64dd0b38dca5098ce9
Chaitanya Agarkar <129309578+chaieeetanya433@users.noreply.github.com>
2024-01-31 20:59:50 +0530

    Update nthFiboNo.java

--- DP/nthFiboNo.java
+++ DP/nthFiboNo.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Solution {
 
+	//top down approach - recursion + memoization
 	public static int fibo(int n, int[] dp) {
 		if(n==1 || n==0) {
 			return n;
@@ -15,6 +16,37 @@ public class Solution {
 		dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
 		return dp[n];
 	}
+	//bottom-up approach - recursion + tabulation
+	/*public static int fibo(int n, int[] dp) {
+		if(n==1 || n==0) {
+			return n;
+		}
+
+		dp[1] = 1;
+		dp[0] = 0;
+		for(int i=2; i<=n; i++) {
+			dp[i] = dp[i-1] + dp[n-2];
+		}
+		return dp[n];
+	}*/
+
+	//space optimized tc=O(1) 
+	/*public static int fib(int n) {
+		int prev1=1;
+		int prev2=0;
+
+		if(n==0) {
+			return prev2;
+		}
+
+		for(int i=2; i<=n; i++) {
+			int curr = prev1 + prev2;
+			prev2 = prev1;
+			prev1 = curr;
+		}
+		return prev1;
+	}*/
+	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
